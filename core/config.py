5871794ee1657555ce7977eaa8b392f62bcd32e1
mrwanismail13-coder <mrwanismail13@gmail.com>
2026-06-04 20:13:17 +0300

    Update config.py

--- core/config.py
+++ core/config.py
@@ -2,17 +2,40 @@ from pathlib import Path
 import sys
 
 
+# ==========================================
+# BASE PATH
+# ==========================================
 def get_base_path():
 
-    # EXE mode
-    if getattr(sys, 'frozen', False):
+    # EXE mode (PyInstaller)
+    if getattr(sys, "frozen", False):
         return Path(sys._MEIPASS)
 
-    # dev mode
+    # Python mode
     return Path(__file__).resolve().parent.parent
 
 
 ROOT_DIR = get_base_path()
 
+# ==========================================
+# MODEL
+# ==========================================
 MODELS_DIR = ROOT_DIR / "models"
+
 MODEL_PATH = MODELS_DIR / "best_v2.pt"
+
+# ==========================================
+# YOLO
+# ==========================================
+YOLO_CONFIDENCE = 0.45
+
+# ==========================================
+# CLASS NAMES
+# ==========================================
+CLASS_CUSHION = "cushion"
+
+CLASS_OBJECT_BALL = "object_ball"
+
+CLASS_POCKET = "pocket"
+
+CLASS_CUE_BALL = "white_cue_ball"
