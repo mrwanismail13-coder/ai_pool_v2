2ebeab158977516b2c240d2c5e2e0cb81363e527
mrwanismail13-coder <mrwanismail13@gmail.com>
2026-06-05 01:01:11 +0300

    Update ball_manager.py

--- core/ball_manager.py
+++ core/ball_manager.py
@@ -4,9 +4,13 @@ class BallManager:
 
         self.object_balls = []
         self.cue_ball = None
+
         self.pockets = []
         self.cushions = []
 
+    # =========================
+    # UPDATE FROM YOLO (FILTERED)
+    # =========================
     def update(self, detections):
 
         self.object_balls = []
@@ -16,29 +20,38 @@ class BallManager:
 
         for det in detections:
 
-            name = det.get("class_name")
-            center = det.get("center")
+            class_name = det.get("class_name")
 
-            if not center:
-                continue
+            # =========================
+            # CUE BALL
+            # =========================
+            if class_name == "white_cue_ball":
+                self.cue_ball = det
 
-            if name == "white_cue_ball":
-                self.cue_ball = center
+            # =========================
+            # OBJECT BALLS
+            # =========================
+            elif class_name == "object_ball":
+                self.object_balls.append(det)
 
-            elif name == "object_ball":
-                self.object_balls.append(center)
+            # =========================
+            # POCKETS
+            # =========================
+            elif class_name == "pocket":
+                self.pockets.append(det)
 
-            elif name == "pocket":
-                self.pockets.append(center)
+            # =========================
+            # CUSHIONS
+            # =========================
+            elif class_name == "cushion":
+                self.cushions.append(det)
 
-            elif name == "cushion":
-                self.cushions.append(center)
-
+    # =========================
     def get_cue_ball(self):
-        return self.cue_ball
+        return self.cue_ball["center"] if self.cue_ball else None
 
     def get_object_balls(self):
-        return self.object_balls
+        return [b["center"] for b in self.object_balls]
 
     def get_pockets(self):
-        return self.pockets
+        return [p["center"] for p in self.pockets]
